ced3e462f1472b1cd9aa0734579d75afa147dd0f
@isaackevin <isaacbasses@outlook.com>
2022-02-05 19:37:20 -0300

    Setando Email

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -18,8 +18,8 @@ def subscribe(request):
             mail.send_mail(
                 'Confirmação de inscrição',
                 body,
-                'contato@eventex.com.br',
-                ['contato@eventex.com.br', form.cleaned_data['email']]
+                'kevin.tecnologia.1@gmail.com',
+                ['kevin.tecnologia.1@gmail.com', form.cleaned_data['email']]
             )
 
             messages.success(request, 'Inscrição realizada com sucesso!')
