546c3ad57963f3b695cc79557e2b8988c884fbbc
@isaackevin <isaacbasses@outlook.com>
2022-02-20 20:36:57 -0300

    change email default

--- eventex/settings.py
+++ eventex/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
-DEFAULT_FROM_EMAIL = 'isaacbasses@outlook.com'
+DEFAULT_FROM_EMAIL = 'isaaclex50@gmail.com'
 ADMINS = [DEFAULT_FROM_EMAIL]
 MANAGERS = ADMINS
 
