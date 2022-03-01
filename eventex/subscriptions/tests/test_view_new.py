6b36200b57b7540bcc1ca3e25298e327c4b7ae2f
isaackevin <isaacbasses@outlook.com>
2022-03-01 17:43:25 -0300

    Envie email ou telefone

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -80,3 +80,10 @@ class SubscriptionNewPostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_fields_erross(self):
+        invalid_data = dict(dict(name='Isaac Kevin', cpf='12345678901'))
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+
+        self.assertContains(response, '<ul class="errorlist nonfield">')
